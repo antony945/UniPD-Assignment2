d630aeb171629d31ddfc28b6086f18c3ae1ca066
Antonio Franzoso <antonio.franzoso@outlook.it>
2021-01-08 15:19:31 +0100

    Sistemato

--- Rail_old/src/RegionalTrain.cpp
+++
@@ -1,6 +0,0 @@
-//Alberto Castagnaro 1219641
-#include "RegionalTrain.h"
-
-RegionalTrain::RegionalTrain(int id_, bool left_, const std::vector<Station *> &stations_,const std::vector<int>& timetable_): Train(id_, left_, stations_,160, timetable_)
-{}
-RegionalTrain::~RegionalTrain() = default;
